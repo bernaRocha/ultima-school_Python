89fe9b83219b24851e0ca44080f794c5c1a3a55b
Bernardo <mrochabernardo@gmail.com>
2022-09-13 18:03:34 -0300

    uso de constantes no exercício do IMC

--- modulo01-questoes/questao6-IMC.py
+++ modulo01-questoes/questao6-IMC.py
@@ -20,28 +20,28 @@ altura = float(input('Qual a sua altura: '))
 
 imc = peso_atual / (altura * altura)
 
-muito_abaixo = imc < 17
-abaixo_peso = imc <= 18.5
-peso_normal = imc <= 25
-sobrepeso = imc <= 30
-obesidade_grau1 = imc <= 35
-obesidade_grau2 = imc <= 40
-obesidade_morbida = imc > 40
+MUITO_ABAIXO = imc < 17
+ABAIXO_PESO = imc <= 18.5
+PESO_NORMAL = imc <= 25
+SOBREPESO = imc <= 30
+OBESIDADE_GRAU1 = imc <= 35
+OBESIDADE_GRAU2 = imc <= 40
+OBESIDADE_MORBIDA = imc > 40
 
 print(f'Seu IMC é de {imc:.1f}')
 
-if muito_abaixo:
+if MUITO_ABAIXO:
     print(f'Seu IMC é {imc}. Seu peso está muito baixo!')
-elif abaixo_peso:
+elif ABAIXO_PESO:
     print(f'Seu IMC é {imc}. Você está abaixo do peso')
-elif peso_normal:
+elif PESO_NORMAL:
     print(f'Seu IMC é {imc}. Você está com o peso normal')
-elif sobrepeso:
+elif SOBREPESO:
     print(f'Seu IMC é {imc}. Você está com sobrepeso')
-elif obesidade_grau1:
+elif OBESIDADE_GRAU1:
     print(f'Seu IMC é {imc}. Você está com obesidade grau 1, cuidado')
-elif obesidade_grau2:
+elif OBESIDADE_GRAU2:
     print(f'Seu IMC é {imc}. Você está com obesidade grau 2, cuidado')
 else:
     print(f'Seu IMC é {imc}. Você está com obesidade mórbida, cuidado')
-  
+      

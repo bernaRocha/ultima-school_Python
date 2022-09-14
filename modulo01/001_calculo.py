db8081d6dfc42cdc25d2148a1181c10751d63851
Bernardo <mrochabernardo@gmail.com>
2022-09-14 17:02:40 -0300

    mais operações de cálculo

--- modulo01/001_calculo.py
+++ modulo01/001_calculo.py
@@ -5,7 +5,15 @@ numero1 = int(input('Informe o primeiro número: '))
 numero2 = int(input('Informe o segundo número: '))
 
 somatorio = numero1 + numero2
+subtracao = numero1 - numero2
 multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+modulo_divisao = numero1 % numero2
+divisao_inteira = numero1 // numero2
 
-print(f'A soma de {numero1} com {numero2} é {somatorio}'
-f'A multiplicação de {numero1} com {numero2} é {multiplicacao}')
+print(f'A soma de {numero1} com {numero2} é {somatorio}\n'
+f'A subtração de {numero1} com {numero2} é {subtracao}\n'
+f'A multiplicação de {numero1} com {numero2} é {multiplicacao}\n'
+f'A divisão de {numero1} com {numero2} é {divisao}' 
+f'O módulo de {numero1} com {numero2} é {modulo_divisao}\n'
+f'A divisão inteira de {numero1} com {numero2} é {divisao_inteira}\n')

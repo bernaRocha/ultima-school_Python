47a7ec14e72cef75dc5a81adefffe3172a4b1f80
Bernardo <mrochabernardo@gmail.com>
2022-09-15 19:01:37 -0300

    mais um exemplo em formatação

--- modulo01-semana1/002_formatacao.py
+++ modulo01-semana1/002_formatacao.py
@@ -1,4 +1,12 @@
 for i in range(100):
     valor = i + 1
     print(f'{valor:03}')
+
+PI = 3.141592
+
+print(f'Valor de PI: {PI:8.2f}') # Valor de PI:     3.14
+
+nome = "Fulano da Silva Ciclano"
+
+print(f'O nome {nome} ao contrário fica: {nome[::-1]}')
     

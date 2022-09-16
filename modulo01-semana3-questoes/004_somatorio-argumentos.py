a77dc1d0d92ea90a5348220754623e54f6c892ae
Bernardo <mrochabernardo@gmail.com>
2022-09-16 13:20:18 -0300

    Somatório de argumentos numéricos

---
+++ modulo01-semana3-questoes/004_somatorio-argumentos.py
@@ -0,0 +1,12 @@
+'''
+4. Crie um programa com uma função que necessite de três argumentos, 
+e que forneça a soma desses três argumentos.
+'''
+from os import system
+system('clear')
+
+def soma_tres(argumento1, argumento2, argumento3):
+    somatorio = argumento1 + argumento2 + argumento3
+    return somatorio
+
+print(f'A soma dos três argumentos é: {soma_tres(1, 3, 5)}') # 9

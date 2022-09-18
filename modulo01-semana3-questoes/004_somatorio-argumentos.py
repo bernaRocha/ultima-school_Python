486df9984c6e373b56b7be47c4d6e7787ba2a747
Bernardo <mrochabernardo@gmail.com>
2022-09-18 10:14:32 -0300

    ex 04 somatório de argumentos/números

--- modulo01-semana3-questoes/004_somatorio-argumentos.py
+++ modulo01-semana3-questoes/004_somatorio-argumentos.py
@@ -10,3 +10,4 @@ def soma_tres(argumento1, argumento2, argumento3):
     return somatorio
 
 print(f'A soma dos três argumentos é: {soma_tres(1, 3, 5)}') # 9
+print(f'A soma dos três argumentos é: {soma_tres(9, 23, 67)}') # 99

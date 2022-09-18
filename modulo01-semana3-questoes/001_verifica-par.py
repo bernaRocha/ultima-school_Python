55081d6fb60bdd4e1280fe0d5bdf655d87566349
Bernardo <mrochabernardo@gmail.com>
2022-09-18 06:16:51 -0300

    refatoração

--- modulo01-semana3-questoes/001_verifica-par.py
+++ modulo01-semana3-questoes/001_verifica-par.py
@@ -5,11 +5,12 @@ retorna se um número  é par ou ímpar (utilizando **kwargs).
 from os import system
 system('clear')
 
-def verifica_par_impar(numero, divisor=2):
-    if numero % divisor == 0:
+def verifica_par_impar(numero):
+    if numero % 2 == 0:
         return f'O número {numero} é par'
     else:
         return f'O número {numero} não é par'
 
-print(verifica_par_impar(8))
+print(verifica_par_impar(8)) # O número 8 é par
+print(verifica_par_impar(9)) # O número 9 não é par
     

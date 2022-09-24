764d79a673092acb70d39b42b9969be2af792768
Bernardo <mrochabernardo@gmail.com>
2022-09-24 09:43:49 -0300

    média com lista

---
+++ lista-de-exercicios-extra/01_estrutura-sequencial/04_media-bimestral.py
@@ -0,0 +1,15 @@
+'''
+Faça um Programa que peça as 4 notas bimestrais e mostre a média. 
+'''
+
+from functools import total_ordering
+from os import system
+system('clear')
+
+notas_aluno = []
+for notas in range(0, 4):
+    notas = notas_aluno.append(float(input('Registre a nota do aluno: ')))
+
+    media = sum(notas_aluno) / 4
+
+print(f'A média do aluno é {media:.2f}')

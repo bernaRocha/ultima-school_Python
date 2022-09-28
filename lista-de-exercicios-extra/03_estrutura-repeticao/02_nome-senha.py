b300eabbecf5f629d336c6989f04bd0ae8459a69
Bernardo <mrochabernardo@gmail.com>
2022-09-28 14:53:24 -0300

    verifica nome e senha

--- lista-de-exercicios-extra/03_estrutura-repeticao/02_nome-senha.py
+++ lista-de-exercicios-extra/03_estrutura-repeticao/02_nome-senha.py
@@ -3,7 +3,7 @@ Faça um programa que leia um nome de usuário e a sua senha e não aceite a
 senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando 
 a pedir as informações. 
 '''
-from ast import While
+
 from os import system
 system('clear')
 

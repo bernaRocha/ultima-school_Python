7889c1f009c4a2a1c88d7cf535a63e2938d6d4c0
Bernardo <mrochabernardo@gmail.com>
2022-09-28 10:32:44 -0300

    comparação de nome e senha

---
+++ lista-de-exercicios-extra/03_estrutura-repeticao/02_nome-senha.py
@@ -0,0 +1,17 @@
+'''
+Faça um programa que leia um nome de usuário e a sua senha e não aceite a 
+senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando 
+a pedir as informações. 
+'''
+from ast import While
+from os import system
+system('clear')
+
+nome = input('Qual seu nome? ')
+senha = input('Registre uma senha: ')
+
+while senha == nome:
+    print('Essa senha é inválida, tente novamente.')
+    senha = input('Registre uma senha: ')
+
+print('Nome e senha cadastradas com sucesso. ')

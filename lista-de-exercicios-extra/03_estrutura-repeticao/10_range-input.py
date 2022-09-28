3870278dc9f6e678e90a1f04053c028be52d83f4
Bernardo <mrochabernardo@gmail.com>
2022-09-28 20:25:02 -0300

    range com input

---
+++ lista-de-exercicios-extra/03_estrutura-repeticao/10_range-input.py
@@ -0,0 +1,13 @@
+'''
+Faça um programa que receba dois números inteiros e gere os números inteiros 
+que estão no intervalo compreendido por eles. 
+'''
+from os import system
+system('clear')
+
+comeco = int(input('Número do começo da lista: '))
+fim = int(input('Número do fim da lista: '))
+
+for i in range(comeco, fim + 1):
+    print(f'{i} - ', end=' ')
+print("FIM")

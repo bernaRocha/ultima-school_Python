234fd5eb204fb85e825fc10168f3db1d0c80a7c6
Bernardo <mrochabernardo@gmail.com>
2022-09-27 22:21:43 -0300

    Math

---
+++ pythonclcoding/17_modulo-math.py
@@ -0,0 +1,20 @@
+import math
+from os import system
+system('clear')
+
+print(math.pi) # 3.141592653589793
+
+print(math.e) # 2.718281828459045
+
+print(math.radians(30)) # 0.5235987755982988
+print(math.radians(45)) # 0.7853981633974483
+print(math.radians(60)) # 1.0471975511965976
+print(math.radians(90)) # 1.5707963267948966
+
+print(math.degrees(math.pi/6)) # 29.999999999999996
+
+print(math.log(10)) # 2.302585092994046
+print(math.log10(10)) # 1.0
+print(math.e**2) # 7.3890560989306495
+print(math.pow(2, 3)) # 8.0
+print(int(math.pow(2, 3))) # 8

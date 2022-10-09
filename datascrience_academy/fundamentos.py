6fb19f3715710cbe91a7dd86de0881e202941c99
Bernardo <mrochabernardo@gmail.com>
2022-10-09 05:36:34 -0300

    del

--- datascrience_academy/fundamentos.py
+++
@@ -1,8 +0,0 @@
-from platform import python_version
-from os import system
-system('clear')
-
-print(f'Versão da linguagem Python: {python_version()}')
-# Versão da linguagem Python: 3.8.10
-
-print(pow(4,2))

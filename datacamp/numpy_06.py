e78171f1d322cb914dc9499514bdba4ce96242c9
Bernardo <mrochabernardo@gmail.com>
2022-10-17 18:16:06 -0300

    Data camp

---
+++ datacamp/numpy_06.py
@@ -0,0 +1,22 @@
+# np_baseball is available
+
+# Import numpy
+import numpy as np
+
+# Print mean height (first column)
+avg = np.mean(np_baseball[:,0])
+print("Average: " + str(avg))
+
+# Print median height. Replace 'None'
+med = np.median(np_baseball[:,0])
+print("Median: " + str(med))
+
+# Print out the standard deviation on height. Replace 'None'
+stddev = np.std(np_baseball[:,0])
+print("Standard Deviation: " + str(stddev))
+
+# Print out correlation between first and second column. Replace 'None'
+corr = np.corrcoef(np_baseball[:,0], np_baseball[:,1])
+print("Correlation: " + str(corr))
+
+# parei aqui https://campus.datacamp.com/courses/intro-to-python-for-data-science/chapter-4-numpy?ex=16

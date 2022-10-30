0ad5cde47b53795dc688208913c0505f2db35f9d
Bernardo <mrochabernardo@gmail.com>
2022-10-29 21:43:34 -0300

    média e conceito do aluno

--- lista-de-exercicios-python-org/02_estruturas-decisao/14_media-conceito.py
+++ lista-de-exercicios-python-org/02_estruturas-decisao/14_media-conceito.py
@@ -7,6 +7,9 @@ Faça um programa que lê as duas notas parciais obtidas por um aluno numa disci
       Entre 6.0 e 7.5         C
       Entre 4.0 e 6.0         D
       Entre 4.0 e zero        E
+
+O algoritmo deve mostrar na tela as notas, a média, o conceito correspondente e a mensagem “APROVADO” 
+se o conceito for A, B ou C ou “REPROVADO” se o conceito for D ou E. 
 '''
 from os import system
 system('clear')
@@ -22,13 +25,18 @@ media = sum(notas) / 2
 
 if media < 4:
     conceito = 'E'
+    situacao = "reprovado"
 elif media < 6:
     conceito = 'D'
+    situacao = "reprovado"
 elif media < 7.5:
     conceito = 'C'
+    situacao = "aprovado"
 elif media < 9:
     conceito = 'B'
+    situacao = "aprovado"
 else:
     conceito = 'A'
+    situacao = "aprovado"
 
-print(f'Tendo a média de: {media} pontos o conceito do aluno é: {conceito}')
+print(f'Tendo a média de: {media:.2f} pontos o conceito do aluno é: {conceito}, sua situação é: {situacao}')

2cc31027963366a76fd85e6a5eb1df21a6537355
Bernardo <mrochabernardo@gmail.com>
2022-11-19 08:02:01 -0300

    padrão rangoli

---
+++ pythonclcoding/21_rangoli.py
@@ -0,0 +1,25 @@
+from os import system
+system('clear')
+
+def rangoli(size):
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    
+    data = [alpha[i] for i in range(size)]
+
+    items = list(range(size))
+
+    items = items[:-1]+items[::-1]
+
+    for i in items:
+
+        temp = data[-(i + 1):]
+
+        row = temp[::-1] + temp[1:]
+
+        print("-".join(row).center(n*4-3,"-"))
+    
+if __name__ == '__main__':
+
+    n = int(input())
+
+    rangoli(n)
